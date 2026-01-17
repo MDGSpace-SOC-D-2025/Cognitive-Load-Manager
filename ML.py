bd17a83678bf99e83e8b0838de36d0095fc7fb60
Aaryapar <ryapargaonkar@gmail.com>
2026-01-17 11:37:50 +0530

    fix merge conflict

--- ML.py
+++ ML.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # import pandas as pd
 # from sklearn.tree import DecisionTreeRegressor
 # import joblib
@@ -22,34 +22,3 @@
 
 
 # joblib.dump(predicting_model, "cognitive_load_model.pkl")
-=======
-import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
-import joblib
-
-
-
-synthetic_data_file_path=r"C:\Users\ryapa\OneDrive\Desktop\CLM\training_data.csv"
-predicting_data=pd.read_csv(synthetic_data_file_path)
-# print(predicting_data.describe())
-
-y=predicting_data.cognitive_load
-
-predicting_features=['sleep_hours', 'fatigue_level', 'assignments_due', 'avg_deadline_days', 'study_hours']
-
-x=predicting_data[predicting_features]
-# print(x.describe())
-
-# predicting_model=DecisionTreeRegressor(random_state=1)
-# predicting_model.fit(x,y)
-
-# print("Making predictions for the following:")
-# print(x.head())
-# print("The predictions are")
-# print(predicting_model.predict(x.head()))
-
-predicting_model=DecisionTreeRegressor()
-predicting_model.fit(x,y)
-
-joblib.dump(predicting_model, "cognitive_load_model.pkl")
->>>>>>> 9c15bd2489283ce4463ab9d773e86d0d0f88124b
--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     google_id TEXT UNIQUE,
@@ -47,60 +47,6 @@ CREATE TABLE IF NOT EXISTS study_sessions(
     end_time DATETIME,
     duration_hours REAL,
     FOREIGN KEY (user_id) REFERENCES users(id)
-
-
-
-=======
-CREATE TABLE IF NOT EXISTS users(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    google_id TEXT UNIQUE,
-    email TEXT,
-    name TEXT
-);
-
-CREATE TABLE IF NOT EXISTS assignments(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
-    title TEXT,
-    deadline TEXT,
-    google_event_id TEXT NOT NULL,
-
-
-    UNIQUE(user_id, google_event_id),
-    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 
-CREATE TABLE IF NOT EXISTS user_state (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
-    date TEXT,
-    sleep_hours REAL, 
-    fatigue_level INTEGER,
-    FOREIGN KEY(user_id) REFERENCES users(id)   
-);
-
-
-CREATE TABLE IF NOT EXISTS cognitive_load(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-user_id INTEGER,
-date TEXT,
-load_score REAL,
-load_label TEXT,
-FOREIGN KEY(user_id) REFERENCES users(id) 
-
-);
-
-
-CREATE TABLE IF NOT EXISTS study_sessions(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
-    start_time DATETIME NOT NULL,
-    end_time DATETIME,
-    duration_hours REAL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-
-
-
->>>>>>> 9c15bd2489283ce4463ab9d773e86d0d0f88124b
-);
