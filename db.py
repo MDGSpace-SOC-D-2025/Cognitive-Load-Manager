989db41a481a4425e83f61cfdea491e3348f2f5e
Aaryapar <ryapargaonkar@gmail.com>
2026-01-17 11:02:10 +0530

    db

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import sqlite3
 
 DB_NAME="cognitive_load.db"
@@ -6,13 +6,4 @@ DB_NAME="cognitive_load.db"
 def get_connection():
     conn=sqlite3.connect(DB_NAME)
     conn.execute("PRAGMA foreign_keys=ON")
-=======
-import sqlite3
-
-DB_NAME="cognitive_load.db"
-
-def get_connection():
-    conn=sqlite3.connect(DB_NAME)
-    conn.execute("PRAGMA foreign_keys=ON")
->>>>>>> 9c15bd2489283ce4463ab9d773e86d0d0f88124b
     return conn
